4166504bb15bfaa6b0b0fa8c88a0e92d9fbe11c4
JimmyHoldo <jimmy_holdo@hotmail.com>
2019-03-11 22:52:34 +0100

    armcpp5s

--- stprg/reader/reader.cpp
+++ stprg/reader/reader.cpp
@@ -40,7 +40,7 @@ int main()
             port.getData(dataStr);
 
             std::cout << dataStr << std::endl;
-            FILE *f = fopen("../../files/test/arm_test_10s.txt", "a");
+            FILE *f = fopen("../../files/test/arm_test_5s.txt", "a");
             if (f == NULL)
             {
                 printf("Error opening file!\n");
