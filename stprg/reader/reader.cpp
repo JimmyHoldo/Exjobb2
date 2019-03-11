3b9c3df42c65f042482ddcde424fa10a1fd62e50
JimmyHoldo <jimmy_holdo@hotmail.com>
2019-03-12 00:21:38 +0100

    armcpp200ms2

--- stprg/reader/reader.cpp
+++ stprg/reader/reader.cpp
@@ -40,7 +40,7 @@ int main()
             port.getData(dataStr);
 
             std::cout << dataStr << std::endl;
-            FILE *f = fopen("../../files/test/arm_test_175ms.txt", "a");
+            FILE *f = fopen("../../files/test/arm_test_200ms.txt", "a");
             if (f == NULL)
             {
                 printf("Error opening file!\n");
